7c10d70a1ca57ce7d3af01a75852b11fadc02d28
yuriilizan95 <yuriilizan@gmail.com>
2026-09-06 13:46:40 +0200

    refactor: move Notification into components directory

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -5,7 +5,7 @@ import css from './App.module.css';
 import type { Votes, VoteType } from '../../types/votes';
 import VoteOptions from '../VoteOptions/VoteOptions';
 import VoteStats from '../VoteStats/VoteStats';
-import Notification from '../../Notification/Notification';
+import Notification from '../Notification/Notification';
 
 
 
